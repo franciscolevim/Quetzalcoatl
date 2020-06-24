42037a4ac1c817e3ddbc970fb0bcc6a880fbac67
Francisco Leví <franciscolevim@gmail.com>
2020-06-24 18:53:23 -0500

    Refactorización de la clase Resources para administrar la lista de paths de recursos.

--- source/quetzalcoatl/resources.py
+++ source/quetzalcoatl/resources.py
@@ -1,32 +1,25 @@
 from quetzalcoatl.environment import Environment
 from quetzalcoatl.utils import path_validators as pathvalid
 
-
-class Paths:
-    """Administra los paths necesarios para acceder a los recursos del juego.
-    """
-    def __init__(self):
-        self.__RESOURCES_DIRECTORY = 'resources'
-        self.__resources = [f'{Environment.paths.home}/{self.__RESOURCES_DIRECTORY}']
-
-
-    @property
-    def list(self):
-        """Lista de paths en los que se pueden encontrar recursos para el juego.
-
-        Puede haber más de una ruta que contenga recursos. Si existe la variable de entorno QUETZALCOATL la ruta default 
-        será QUETZALCOATL/RESOURCES_DIRECTORY;  de lo contrario la ruta default será 
-        QUETZALCOATL_HOME_DIRECTORY/RESOURCES_DIRECTORY.
-        """
-        return self.__resources
+from pyglet.resource import Loader
 
 
 class Resources:
-    """Funcionalidades de los recursos del juego.
+    """Aministra los recursos necesarios para el juego.
+
+    Puede haber más de una ruta que contenga recursos.
     """
-    paths = Paths()
+    __loader = Loader()
+
 
     @staticmethod
-    def add(path:str):
-        if pathvalid.exists(path) and path not in Resources.paths.list:
-            Resources.paths.list.append(path)
+    def add_path(path:str):
+        if pathvalid.exists(path) and path not in Resources.__loader.path:
+            Resources.__loader.path.append(path)
+            Resources.__loader.reindex()
+
+
+    @staticmethod
+    def remove_path(path:str):
+        Resources.__loader.path.remove(path)
+        Resources.__loader.reindex()
