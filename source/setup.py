7cee90c45d8c96fb8578336e4704e474b1f8fd60
Francisco Leví <franciscolevim@gmail.com>
2020-06-24 12:15:58 -0500

    Estrucutra principal del proyecto.

---
+++ source/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name = 'Qutzalcoatl',
+    version = '1.0',
+    author = 'Francisco Leví',
+    author_email = 'franciscolevim@gmail.com',
+    url = 'https://github.com/franciscolevim/Quetzalcoatl',
+    packages = ['quetzalcoatl']
+)
