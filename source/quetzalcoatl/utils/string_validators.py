b9047d9757d838c4c11a421d143599a50c16ff46
Francisco Leví <franciscolevim@gmail.com>
2020-06-24 14:30:40 -0500

    Validar si un texto es un valor vacío.

---
+++ source/quetzalcoatl/utils/string_validators.py
@@ -0,0 +1,13 @@
+"""Utilidades para validar de distintas maneras un txto según sea necesario.
+"""
+
+def is_empty(value:str):
+    """Valida si un texto tiene un valor None o una cadena en blanco o vacía.
+
+    Args:
+        value (str): texto que se desea validar.
+
+    Returns:
+        bool: Si el texto no es None ni una cadena en blanco o vacía devuelve False, de lo contrario devuelve True.
+    """
+    return not (value and value.strip() != '')
