d7b71f9cb6b89ad6ea73358d7ff0234540849480
Francisco Leví <franciscolevim@gmail.com>
2020-06-25 13:49:42 -0500

    Pruebas del ejecutable.

--- source/main.py
+++ source/main.py
@@ -1,13 +1,17 @@
+from quetzalcoatl.resources import Resources
+from quetzalcoatl.environment import Environment
+from quetzalcoatl.stage.basic_layer import BasicLayer
 from cocos.director import director
 from cocos.scene import Scene
 from cocos.layer import Layer
-from cocos.sprite import Sprite
+
 
 
 if __name__ == "__main__":
     """Ejecución del famoso juego de la vibora representado por Quetzalcoatl.
     """
-    director.init(caption = 'Quetzalcoatl')
-    layer = Layer()
+    Resources.load_sprites_path()
+    director.init(caption = 'Quetzalcoatl', width = 800, height = 600)
+    layer = BasicLayer()
     scene = Scene(layer)
     director.run(scene)
